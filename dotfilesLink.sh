4e03e55abac7966896c96f856cf678883be870ad
Yudedako <yudedako.red@gmail.com>
2018-08-24 01:16:14 +0900

    docs: @TODO: の追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# @TODO: .git以外のファイルをfor文で回したらファイル追加時に、このスクリプトに追加する必要がなくなる
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.inputrc ~/.inputrc
